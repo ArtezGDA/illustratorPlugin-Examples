c3bb55dcbb0a344d2e5515dea1a064337ba03ca7
irlabs <developer@irlabs.nl>
2016-05-05 16:01:10 +0200

    Step 1: adding greyish stroke. (Still is much easier done in Illustrator itself)

--- expandedOutlines/modifyExistingFile.jsx
+++ expandedOutlines/modifyExistingFile.jsx
@@ -1,31 +1,39 @@
 /* Expand Outlines - Multiple */
 
-function duplicateOutline(obj, strokeW, color) {
+function duplicateOutline(compound, strokeW, color) {
+	var paths = compound.pathItems;
+	$.writeln (paths.length);
+	for (k = 0; k < paths.length; k++) {
+		$.writeln(paths[k]);
+		var path = paths[k];
+		path.strokeColor = color;
+		path.fillColor = color;
+		path.strokeWidth = strokeW;
+	}
 }
 function extraOutlines(objectGroup, strokeW, color) {
+	
+	var compounds = objectGroup.compoundPathItems;
+	for (j = 0; j < compounds.length; j++) {
+		$.writeln(compounds[j]);
+		var compoundObj = compounds[j];
+		
+		duplicateOutline(compoundObj, strokeW, color);
+	}
 }
 function buildOutlineStack(objectGroup) {
-		$.writeln (objectGroup);
-		var groups  = objectGroup.groupItems;
-		for (i = 0; i < groups.length; i++) {
-			$.writeln (groups[i]);
-			var group = groups[i];
-			
-			var compounds = group.compoundPathItems;
-			for (j = 0; j < compounds.length; j++) {
-				$.writeln(compounds[j]);
-				var compound = compounds[j];
-				
-				var paths = compound.pathItems;
-				$.writeln (paths.length);
-				for (k = 0; k < paths.length; k++) {
-					$.writeln(paths[k]);
-					var path = paths[k];
-					path.strokeColor = path.fillColor;
-					path.strokeWidth = 25.0;
-				}
-			}
-		}
+	$.writeln (objectGroup);
+	var groups  = objectGroup.groupItems;
+	for (i = 0; i < groups.length; i++) {
+		$.writeln (groups[i]);
+		var group = groups[i];
+		
+		var grey = new RGBColor();
+		grey.red = 144;
+		grey.green = 172;
+		grey.blue = 169;
+		extraOutlines(group, 20.0, grey);
+	}
 }
 
 var doc = app.activeDocument;
