dc0a62f4a8ed3c1a73c4a457d829f599ec2ebc4d
irlabs <developer@irlabs.nl>
2016-05-05 15:56:34 +0200

    Printing the group(layer)

--- expandedOutlines/modifyExistingFile.jsx
+++ expandedOutlines/modifyExistingFile.jsx
@@ -5,8 +5,14 @@ function duplicateOutline(obj, strokeW, color) {
 function extraOutlines(objectGroup, strokeW, color) {
 }
 function buildOutlineStack(objectGroup) {
+		$.writeln (objectGroup);
+		var groups  = objectGroup.groupItems;
+		for (i = 0; i < groups.length; i++) {
+			$.writeln (groups[i]);
+		}
 }
 
 var doc = app.activeDocument;
 var elements = doc.layers[0];
 $.writeln (elements);
+buildOutlineStack(elements);
