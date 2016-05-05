2c131296bf37c5f83378419106958454823b77b6
irlabs <developer@irlabs.nl>
2016-05-05 15:54:17 +0200

    Init - Basic Setup (reinit for better git history)

---
+++ expandedOutlines/modifyExistingFile.jsx
@@ -0,0 +1,12 @@
+/* Expand Outlines - Multiple */
+
+function duplicateOutline(obj, strokeW, color) {
+}
+function extraOutlines(objectGroup, strokeW, color) {
+}
+function buildOutlineStack(objectGroup) {
+}
+
+var doc = app.activeDocument;
+var elements = doc.layers[0];
+$.writeln (elements);
