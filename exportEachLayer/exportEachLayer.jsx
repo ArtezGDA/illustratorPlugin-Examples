6cbcc9a944858d2506c46ceb4762985dc3e927ee
irlabs <developer@irlabs.nl>
2016-05-16 15:14:39 +0200

    Start with logging to Alert and Javascript console

--- exportEachLayer/exportEachLayer.jsx
+++ exportEachLayer/exportEachLayer.jsx
@@ -1,2 +1,12 @@
 // Export Each Layer as SVG
 
+// Get the active document
+if ( app.documents.length > 0 ) {
+	var doc = app.activeDocument;
+	
+	// Display dialog to the user
+	alert(doc)
+	
+	// Print to Javascript console of the ExtendScript ToolKit
+	$.writeln(doc)
+}
