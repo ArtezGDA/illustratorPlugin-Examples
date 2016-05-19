448357d770383cdfdf1379eb3cbb3bc072f95007
irlabs <developer@irlabs.nl>
2016-05-19 08:23:38 +0200

    Logging the name of each layer

--- importJsonData/importJsonData.jsx
+++ importJsonData/importJsonData.jsx
@@ -4,4 +4,12 @@
 if ( app.documents.length > 0 ) {
 	var doc = app.activeDocument;
 	
+	// Get the text layer
+	var layers = doc.layers;
+	if ( layers.length > 0 ) {
+		for (var i = 0; i < layers.length; i++) {
+			var layer = layers[i];
+			$.writeln(layer.name)
+		}
+	}
 }
